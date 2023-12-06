992e4747022f777628bbf5a3fd32d3d5602d6732
LATIFARYES <latifaryes@gmail.com>
2023-12-06 15:22:40 +0300

    ADD M,AINPAGE

--- CenoApp/MAINPAGE.swift
+++ CenoApp/MAINPAGE.swift
@@ -8,11 +8,98 @@
 import SwiftUI
 
 struct MAINPAGE: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    
+@State var showSheet = false
+    
+var body: some View {
 
+    NavigationStack {
+        
+        ZStack{
+
+            Color("ColorBack")
+                .ignoresSafeArea()
+            
+                .navigationTitle(" Explore ")
+                
+                .navigationBarTitleDisplayMode(.large)
+                
+             VStack(spacing: 10){
+                 Text("L")
+                Text("Paths")
+                    .font(.system(size: 28))
+                  .fontWeight(.bold)
+                  .foregroundColor(.black.opacity(12))
+                  .frame(alignment: .topLeading)
+                  .padding(.trailing, 250)
+                  .padding(.top, 40)
+                
+                Rectangle()
+                    .frame(width: 340,height: 350)
+                    .cornerRadius(25)
+                    .foregroundColor(.white)
+                    .shadow(radius: 3.55)
+                    .overlay(
+                        
+                        VStack{
+                            Image("البيت")
+                            Button(action: {
+                                
+                            }) {
+                                
+                                
+                                Text("Play")
+                                    .font(Font.custom("Mulish",
+                                     size: 20).weight(.semibold))
+                                    .multilineTextAlignment(.center)
+                                    .foregroundColor(.white)
+                                    .frame(width: 227, height: 50)
+                                    .background(.colorButtons)
+                                
+                                    .cornerRadius(10)
+                            }
+                            
+                        } .padding(.bottom, 50)
+                        
+                    )
+                 
+                Text("Achievement")
+                    .font(.system(size: 28))
+                  .fontWeight(.bold)
+                  .foregroundColor(.black.opacity(12))
+                  //.frame(alignment: .topLeading)
+                  .padding(.trailing, 159)
+                  .padding(.top, 43.5)
+                  .padding(.horizontal,29)
+                
+                    HStack(spacing: 10){
+                       
+                        
+                Image("Win2")
+                    .resizable()
+                    .frame(width: 120, height: 119)
+                
+                Image("Win1")
+                    .resizable()
+                    .frame(width: 120, height: 119)
+                
+                Image("Win3")
+                    .resizable()
+                    .frame(width: 120, height: 119)
+                
+            }
+
+            }
+            
+            .padding(Edge.Set.vertical,-350)
+            
+        }
+        
+    }
+    
+    
+}
+}
 #Preview {
-    MAINPAGE()
+MAINPAGE()
 }
