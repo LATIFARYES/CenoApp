d0a71799f676ff6efc39670aac801bf0012112be
LATIFARYES <latifaryes@gmail.com>
2023-12-06 14:17:02 +0300

    MAINPAGE

---
+++ CenoApp/MAINPAGE.swift
@@ -0,0 +1,18 @@
+//
+//  MAINPAGE.swift
+//  CenoApp
+//
+//  Created by LATIFA on 06/12/2023.
+//
+
+import SwiftUI
+
+struct MAINPAGE: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MAINPAGE()
+}
